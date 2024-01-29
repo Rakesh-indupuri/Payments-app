deceb5706c796acdee3b7de124edf9a7307f471e
Rakesh-indupuri <rakeshindupuri@gmail.com>
2024-01-29 08:15:57 +0530

    changed cors origin

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,13 @@ const cors=require("cors")
 const app=express()
 const mainRouter=require("./routes/index")
 app.use(express.json())
-app.use(cors())
+// app.use(cors())
+app.use(cors({
+    origin: ["https://payments-app-lovat.vercel.app/"],
+    methods:["POST","GET","PUT"],
+    credentials:true
+  }));
+  
 app.use("/api/v1/",mainRouter)
 
 app.listen(3000)
