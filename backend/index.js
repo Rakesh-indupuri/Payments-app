40b1fac7913b249bb0a7284ccc43253798d799db
Rakesh-indupuri <rakeshindupuri@gmail.com>
2024-09-23 19:22:41 +0530

    Changed Cors

--- backend/index.js
+++ backend/index.js
@@ -3,17 +3,6 @@ const cors=require("cors")
 const app=express()
 const mainRouter=require("./routes/index")
 app.use(express.json())
-// app.use(cors())
-app.use(cors({
-    origin: ["https://payments-app-lovat.vercel.app"],
-    methods:["POST","GET","PUT"],
-    credentials:true
-  }));
-app.options('/api/v1/user/signin', (req, res) => {
-    res.header('Access-Control-Allow-Origin', 'https://payments-app-lovat.vercel.app');
-    res.header('Access-Control-Allow-Methods', 'POST, GET, PUT');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
-    res.status(200).end();
-});
+app.use(cors())
 app.use("/api/v1/",mainRouter)
 app.listen(3000)
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,7 +2,6 @@ const express=require("express")
 const userRouter=require("./user")
 const amountRouter=require("./account")
 const router=express.Router()
-
 router.use("/user",userRouter)
 router.use("/account",amountRouter)
 module.exports=router
